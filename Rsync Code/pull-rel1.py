f1bbd1f752ad273f0d61c14009d135a4e42bb91b
Moaz Mansour <31941060+MoazMansour@users.noreply.github.com>
2018-12-07 12:30:07 -0500

    - Now target bucket and root folders on gcloud and NAS are just two variables to ease changes -Added documentation and explaination to the code

--- Rsync Code/pull-rel1.py
+++ Rsync Code/pull-rel1.py
@@ -1,44 +1,57 @@
 import time
-from subprocess import call #import subprocess library to run bash commands in script
-from subprocess import check_output
+from subprocess import call 									#import subprocess library to run bash commands in script
 from google.cloud import pubsub_v1
 
-project_id = "production-backup-194719"
-subscription_name = "mySub"
+####### Information to be changed based on the type of service an d bucket used ####
+project_id = "production-backup-194719" 						#The project I am assigned to on Gcloud
+subscription_name = "mySub" 									#Pull subscription channel created to pull all object changes messages
+bucket = "gs://rsync-trigger-test/" 							#Bucket path
+g_root = "GSync/" 												#root folder subject to change on the cloud
+l_root = "/rsync-test/"											#root folder subject to change on the local server
 
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_name)
 
+																#Function used to analyze received messages and take actions based on that
 def callback(message):
 	#print('\n Received message: {}'.format(message.data))
-	if message.attributes:
+	if message.attributes:                                     	#read message attributes
 	   #print('\nAttributes:')
 	   for key in message.attributes:
 	       value = message.attributes.get(key)
 	       #print('{}: {}'.format(key, value))
-	       if(key == "eventType"):
+	       if(key == "eventType"):  							#store event type
 			   event = value
-	       elif(key == "objectId"):
+	       elif(key == "objectId"): 							#store path and filename
 			   object = value
-	fsplit = object.rfind('/')+1
-	psplit = object.find('/')+1
-	file = object[fsplit:]
-	path = object[psplit:fsplit]
-	dir = path[:-1]
-	if file:
-		if event == "OBJECT_FINALIZE":
-			call(["mkdir","-p","/rsync-test/"+dir])
-	 		#call(["gsutil","-m","rsync","gs://rsync-trigger-test/GSync/"+path,"/rsync-test/"+path])
-			call(["gsutil","cp","gs://rsync-trigger-test/GSync/"+path+file,"/rsync-test/"+path+file])
-		elif event == "OBJECT_DELETE":
-			call(["rm","/rsync-test/"+path+file])
-			call(["find","/rsync-test/"+path,"-type","d","-empty","-delete"])
-	else:
-		if event == "OBJECT_FINALIZE":
-			call(["mkdir","/rsync-test/"+dir])
-		elif event == "OBJECT_DELETE":
-			call(["find","/rsync-test/"+path,"-type","d","-empty","-delete"])
-	message.ack()
+
+### Parse filename, path, and directory from the objectID
+	fsplit = object.rfind('/')+1								#finding filename position in objectID
+	psplit = object.find('/')+1									#finding full path position exlcuding the root folder
+	file = object[fsplit:]										#Assigning the filename to a variable (will be empty of action was taken on a folder)
+	path = object[psplit:fsplit]								#Assigning path to a variable
+	dir = path[:-1]												#excluding last "/" from path
+
+### Check the type of change and acting upon it
+	if file:													#checks if the action was taken on a file object (which is mostly the case with gcloud)
+		if event == "OBJECT_FINALIZE":							#if file has been created or modified
+			call(["mkdir","-p",l_root+dir])						#assures that the target directory (full path) exists on NAS
+			call(["gsutil","cp",bucket+g_root+path+file,l_root+path+file])     #copies the changed/created file to its destination on NAS
+		elif event == "OBJECT_DELETE":							#checks if file has been deleted or renamed
+			call(["rm",l_root+path+file])						#removes file from NAS
+			call(["find",l_root+path,"-type","d","-empty","-delete"]) #if emptied removes the target folder and its empty subordinates to comply with gcloud object logic
+###
+	else:														#if the file is empty it means it was a folder action (usually a new empty folder has been created or deleted)
+		if event == "OBJECT_FINALIZE":							#checks if folder has been created
+			call(["mkdir","-p",l_root+dir])						#creates the new folder and all its parents if needed
+		elif event == "OBJECT_DELETE":							#checks if folder has been deleted
+			call(["find",l_root+path,"-type","d","-empty","-delete"]) #removes the target folder and its empty subordinates to comply with gcloud object logic
+
+#### End of object changes actions
+	message.ack()												# Sends ack notification to google that message gas been received
+### End of function
+
+### Calls the function whenever a message is received
 subscriber.subscribe(subscription_path, callback=callback)
 
 # The subscriber is non-blocking, so we must keep the main thread from
