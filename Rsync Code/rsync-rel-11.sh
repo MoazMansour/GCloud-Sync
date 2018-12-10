acf62ba3079af5deb4e1b5547b197a9320dc5d44
Moaz Mansour <31941060+MoazMansour@users.noreply.github.com>
2018-12-10 12:49:57 -0500

    Adding variables Added function to check change owner Created sync function instead of having it in main

--- Rsync Code/rsync-rel-11.sh
+++ Rsync Code/rsync-rel-11.sh
@@ -1,51 +1,62 @@
 #! /bin/bash
 
-#specifying kind of events to be monitored
-EVENTS="CREATE,DELETE,MOVED_TO,MOVED_FROM"
+EVENTS="CREATE,DELETE,MOVED_TO,MOVED_FROM"          #specifying kind of events to be monitored
+bucket="gs://production-backup-master/" 						#Bucket path
+g_root="GSync Test/" 												        #root folder subject to change on the cloud
+l_root="/rsync-test/"											          #root folder subject to change on the local server
 
+#############################################
+#Main synchronization function
+function run_sync {
+  path="$1"                                         #passing path to the function
+  folder="$2"                                       #passing folder to the function
+  event="$3"                                        #passing event to the function
+  file="$4"                                         #passing file to the function
+
+# If the object changed was a directory then copy a dummy file into the bucket to create the folder
+  if [[ $event == *"ISDIR"* ]]; then                                              #check directory change
+    if [[ $event == *"CREATE"* ]] || [[ $event == *"MOVED_TO"* ]]; then           #check creating types of changes
+      gsutil cp dummy "$bucket$g_root$folder$file/.initate"                       #creates a dummy file to create a folder on the cloud
+    else
+     if [[ $event == *"DELETE"* ]] || [[ $event == *"MOVED_FROM"* ]]; then        #check deleting types of changes
+        gsutil rm -r "$bucket$g_root$folder$file"                                 #remove folder recersuively from cloud
+     fi
+    fi
+  else
+    #If change was not a directory change the below are the checks run per file change
+    if [[ $event == "CREATE" ]] || [[ $event == "MOVED_TO" ]]; then                #check creation types of changes
+      gsutil cp "$path$file" "$bucket$g_root$folder$file"
+    else
+      if [[ $event == "DELETE" ]] || [[ $event == "MOVED_FROM" ]]; then             #check deletion types of changes
+        gsutil rm "$bucket$g_root$folder$file"                                      #delete only this specific file
+      fi
+    fi
+  fi
+}
+
+#############################################
 #The while is to read the output that comes out from the inotifywatch which is monitoring all the above events
 # The line is in this format "PATH CHANGE_TIME EVENT_TYPE FILE/FOLDER(OBJECT)"
-
 #read lines recersuively into string variable line
 while read -r line
 do
-   printf "CHANGE LOG: $line\n"  #print recevied message on screen
-   path=${line%/*}               #Parsing the path variable from the change message
+   printf "CHANGE LOG: $line\n"                                                       #print recevied message on screen
+   path=${line%/*}                                                                    #Parsing the path variable from the change message
    path="$path/"
-   printf "PATH: $path\n"
-   folder=${path##*/rsync-test/}  #Exclduing the root folder "rsync-test" from path for sync purposes
-   printf "FOLDER: $folder\n"
-   rest=${line##*/}              #reading the rest of the message except the path
-   #printf "REST: $rest\n"
-   read hour event file <<<"${rest}" #reading the change_time event_type and subjected obiect of change
-   ##printf "%s %s %s\n" "HOUR:$hour EVENT:$event FILE:$file"
+   folder=${path##*"$l_root"}                                                         #Exclduing the root folder "rsync-test" from path for sync purposes
+   rest=${line##*/}                                                                   #reading the rest of the message except the path
+   read hour event file <<<"${rest}"                                                  #reading the change_time event_type and subjected obiect of change
 
-# If the object changed was a directory then copy a dummy file into the bucket to create the folder
-   if [[ $event == *"ISDIR"* ]]; then #check directory change
-      #printf "ISDIR Done\n"
-      if [[ $event == *"CREATE"* ]] || [[ $event == *"MOVED_TO"* ]]; then #check creating types of changes
-         #printf "FOLDER COPY RUN\n"
-
-         #To adapt with the flat nature of the bucket this creates a dummy file to create a folder
-         gsutil cp dummy "gs://production-backup-master/GSync Test/$folder$file/.initate"
-      else
-         if [[ $event == *"DELETE"* ]] || [[ $event == *"MOVED_FROM"* ]]; then #check deleting types of changes
-            #printf "FOLDER REOMVE RUN\n"
-            gsutil rm -r "gs://production-backup-master/GSync Test/$folder$file" #remove folder recersuively from cloud
-         fi
-      fi
+   #####
+   # Check if it was a local or remote change to run sync
+   uname="$(stat --format '%U' "$path$file")"                                         #extract owner of file
+   if [ "${uname}" = "root" ]; then                                                   #if root is owner then change was local
+     run_sync "$path" "$folder" "$event" "$file"                                      #call the sync function
    else
-      #If change was not a directory change the below are the checks run per file change
-      if [[ $event == "CREATE" ]] || [[ $event == "MOVED_TO" ]]; then  #check creation types of changes
-         #printf "File RSYNC RUN\n"
-         #The only instant where rsync is run to capture all the changes on all the files for the specific path
-         gsutil -m rsync "$path" "gs://production-backup-master/GSync Test/$folder"
-      else
-        if [[ $event == "DELETE" ]] || [[ $event == "MOVED_FROM" ]]; then  #check deletion types of changes
-           #printf "File DELETE RUN\n"
-           gsutil rm "gs://production-backup-master/GSync Test/$folder$file" #delete only this specific file
-        fi
-      fi
+     printf "Test Failed\n"                                                           #Do nothing
    fi
+   #####
+
 ## End of while loop and calling the inotify watch to monitor changes on rsync-test root folder
-done < <(inotifywait -e "$EVENTS" -m -r --timefmt '%H:%M' --format '%w %T %e %f' /rsync-test/)
+done < <(inotifywait -e "$EVENTS" -m -r --timefmt '%H:%M' --format '%w %T %e %f' "$l_root")
+#############################################
