a00c650dc44bf42ab866d5d4b7720d39329532e0
Moaz <moaz.mansour@blink.la>
2018-12-13 18:00:40 -0500

    ReadyForClinet add

---
+++ Airbnb Automation/ReadyforClient.ps1
@@ -0,0 +1,37 @@
+﻿
+#Folders paths parameters
+$csv_path = "C:\Users\Blink Workstation\Downloads\photo_sets.csv"
+$Index_txt = "C:\Users\Blink Workstation\Downloads\DeliveryCheck.txt"
+$client_path = "Z:\06 - Ready for Client"
+
+#initate a list for photosets
+$SetID = @()
+
+#read Set Ids from the CSV file
+Import-Csv $csv_path |`
+    ForEach-Object {
+        $SetID += $_."Photo set ID"
+    }
+
+
+#Clear file and add new header
+Set-Content -Path $Index_txt -Value ">>>Delivery Check Sheet<<< `r`n"
+Add-Content -Path $Index_txt -Value ">Total Number of Folders:"
+
+#Count number of folders
+Get-ChildItem -Directory $client_path | Measure-Object | %{$_.Count} | Add-Content $Index_txt
+
+#Add some space
+Add-Content -Path $Index_txt -Value "`r"
+
+#Count number of sets on the CS
+Add-Content -Path $Index_txt -Value ">Total Number of Reported Asets:"
+$SetID | Measure-Object | %{$_.Count} | Add-Content $Index_txt
+
+$inputNumber = Read-Host -Prompt "SetID"
+
+if ($SetID -like "$inputNumber*")
+    {
+    Write-Host "True!"
+    } 
+    else { Write-Host "False" }
