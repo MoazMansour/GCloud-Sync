c6aafa4e706645eee703c8281aac19bb70f734dd
Moaz Mansour <31941060+MoazMansour@users.noreply.github.com>
2018-12-17 14:31:10 -0500

    Rsync work (queue functions added)

--- Rsync Code/rsync-rel-12.sh
+++ Rsync Code/rsync-rel-12.sh
@@ -1,8 +1,8 @@
 ###############################################################################
-# NAME:      rsync-rel-11.sh
+# NAME:      rsync-rel-12.sh
 # AUTHOR:    Moaz Mansour, Blink
 # E-MAIL:    moaz.mansour@blink.la
-# DATE:      12/12/2018
+# DATE:      12/17/2018
 # LANG:      Bash Script
 #
 # This script manages monitoring changes on Google Cloud and updating
@@ -11,6 +11,7 @@
 # VERSION HISTORY:
 # 1.0    12/10/2018		  Initial Version
 # 1.1    12/12/2018    	Exlcuded rsync
+# 1.2    12/17/2018    	Adding a queue function
 ###############################################################################
 
 ##############################################################
@@ -24,6 +25,17 @@ EVENTS="CREATE,DELETE,MOVED_TO,MOVED_FROM"          #specifying kind of events t
 bucket="gs://rsync-trigger-test/" 						      #Bucket path
 g_root="GSync/" 												            #root folder subject to change on the cloud
 l_root="/rsync-test/"											          #root folder subject to change on the local server
+proc=0                                              #counter to control number of running procceses
+max_proc=7                                         #set max number of allowed proccesses at once
+
+#############################################
+#Queue function
+function proc_control {
+  proc=$(( proc+1 ))
+  if [[ $proc > max_proc ]]; then
+    wait
+  fi
+}
 
 #############################################
 #Main synchronization function
@@ -36,27 +48,44 @@ function run_sync {
 # If the object changed was a directory then copy a dummy file into the bucket to create the folder
   if [[ $event == *"ISDIR"* ]]; then                                                          #check directory change
     if [[ $event == *"CREATE"* ]]; then                       #check creating types of changes
-      gsutil -m cp -P dummy "$bucket$g_root$folder$file/.initate"··
+      proc_control&
+      gsutil -m cp -P dummy "$bucket$g_root$folder$file/.initate"
       gsutil -m cp -P dummy "$l_root$folder$file/.initate"                                   #creates a dummy file to create a folder on the cloud
+      proc=$(( proc-1 ))
+      trap "kill 0" EXIT
     else
       if [[ $event == *"MOVED_TO"* ]]; then
-        gsutil -m rsync -r -P "$l_root$folder$file" "$bucket$g_root$folder$file"
+        proc_control&
+        gsutil -m rsync -r -P "$l_root$folder$file" "$bucket$g_root$folder$file"&
+        proc=$(( proc-1 ))
+        trap "kill 0" EXIT
       else
         if [[ $event == *"DELETE"* ]] || [[ $event == *"MOVED_FROM"* ]]; then                    #check deleting types of changes
-          gsutil -m rm -r "$bucket$g_root$folder$file"                                             #remove folder recersuively from cloud
+          proc_control&
+          gsutil -m rm -r "$bucket$g_root$folder$file"&                                             #remove folder recersuively from cloud
+          proc=$(( proc-1 ))
+          trap "kill 0" EXIT
         fi
       fi
     fi
   else
     #If change was not a directory change the below are the checks run per file change
     if [[ $event == "CREATE" ]] || [[ $event == "MOVED_TO" ]]; then                           #check creation types of changes
-      gsutil -m cp -P "$path$file" "$bucket$g_root$folder$file"
+      proc_control&
+      gsutil -m cp -P "$path$file" "$bucket$g_root$folder$file"&
+      proc=$(( proc-1 ))
+      trap "kill 0" EXIT
     else
       if [[ $event == "DELETE" ]] || [[ $event == "MOVED_FROM" ]]; then                       #check deletion types of changes
-        gsutil -m rm "$bucket$g_root$folder$file"                                                #delete only this specific file
+        proc_control&
+        gsutil -m rm "$bucket$g_root$folder$file"&                                                #delete only this specific file
+        proc=$(( proc-1 ))
+        trap "kill 0" EXIT
       fi
     fi
   fi
+  proc=$(( proc-1 ))
+  trap "kill 0" EXIT
 }
 
 #############################################
@@ -78,20 +107,25 @@ function callback() {
   if [[ $event == *"CREATE"* ]] || [[ $event == *"MOVED_TO"* ]]; then                 #check if it was a create to get owner
   uname="$(stat --format '%U' "$path$file")"                                         #extract owner of file
   if [ "${uname}" = "root" ]; then                                                   #if root is owner then change was local
-    run_sync "$path" "$folder" "$event" "$file"                                      #call the sync function
+    proc_control&
+    run_sync "$path" "$folder" "$event" "$file"&                                     #call the sync function
   fi
   else
     if [[ $event == *"DELETE"* ]] || [[ $event == *"MOVED_FROM"* ]]; then                #check if it was a deletion
+      proc_control&
       run_sync "$path" "$folder" "$event" "$file"&                                    #run the sync function
     fi
   fi
+  proc=$(( proc-1 ))
+  trap "kill 0" EXIT
   ######
   ## End of while loop and calling the inotify watch to monitor changes on rsync-test root folder
 }
 while read -r line
 do
   printf "CHANGE LOG: $line\n"                                                       #print recevied message on screen
-  callback "$line"&                                                                   #call the callback function
+  proc_control&
+  callback "$line"&                                                                  #call the callback function
 done< <(inotifywait -e "$EVENTS" -m -r --timefmt '%H:%M' --format '%w %T %e %f' "$l_root")
 
 #############################################
