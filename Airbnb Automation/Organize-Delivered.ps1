dc3c6c695b909493fce7067bd42c67a91308219c
Moaz Mansour <moaz.mansour@gmail.com>
2019-01-04 18:35:14 -0500

    Organize-Delivered

---
+++ Airbnb Automation/Organize-Delivered.ps1
@@ -0,0 +1,15 @@
+﻿$path = "C:\Users\Blink Workstation\Desktop\New folder"
+$listings = @()
+$folders = @()
+$folders = Get-ChildItem -Directory $path -Exclude "Home*"
+$listings = Get-ChildItem -Directory $path -Filter "Home*"
+ForEach ($list in $listings){
+    Write-Host "$($list): " -NoNewline
+    Write-Host $list.LastWriteTime
+    $pattern = '(\d+/\d+/\d\d\d\d)'
+    $check = $list.LastWriteTime -match $pattern
+    $mdate = $matches[0]
+    $mdate = $mdate-replace "/", "-"
+
+    Write-Host $mdate
+}
